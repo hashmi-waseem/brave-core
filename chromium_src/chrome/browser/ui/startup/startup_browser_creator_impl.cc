f6910ee1572f4152c33682677d5fde85c3ce1544
Claudio DeSouza <cdesouza@brave.com>
2024-05-02 16:49:57 +0100

    Keep first run experience disabled
    Upstream has deleted all code around `welcome_enabled`, which was being
    set always to true, to disable the first run dialogue from chromium, as
    brave has its own dialogue.
    
    With the deletion of this alternative codepath, the override has been
    updated to directly disable the first run page.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/9813104eb5d5b3a09c67d0ab473de6adb679d74b
    
    commit 9813104eb5d5b3a09c67d0ab473de6adb679d74b
    Author: Jack Yammine <jyammine@google.com>
    Date:   Tue Apr 30 13:43:46 2024 +0000
    
        Reland Remove kForYouFre feature flag check from startup_browser_creator_impl
    
        The feature is now fully launched. This is part of the cleanup process.
    
        Reverted CL: https://crrev.com/c/5481574
        The cause of the revert was fixed in https://crrev.com/c/5490294
    
        Fixed: b:336748320

--- chromium_src/chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chromium_src/chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -3,8 +3,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// This override is in place to make sure the first run page from chromium is
+// not shown, as brave has its own first run page
 #define BRAVE_STARTUPBROWSERCREATORIMPL_DETERMINEURLSANDLAUNCH \
-  welcome_enabled = true;
+  has_first_run_experience = false;
 
 #include "src/chrome/browser/ui/startup/startup_browser_creator_impl.cc"
 #undef BRAVE_STARTUPBROWSERCREATORIMPL_DETERMINEURLSANDLAUNCH
